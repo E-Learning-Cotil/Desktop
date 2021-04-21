c805154c566a5aa3415a7e865d70dbb706ab6ade
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-04-20 23:40:21 -0300

    Criação de uma classe para armazenar as cores usadas no projeto

---
+++ Colors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElearningDesktop
+{
+    class Colors
+    {
+        public static Color lightGray = Color.FromArgb(80, 80, 80);
+        public static Color darkGray = Color.FromArgb(61, 61, 61);
+        public static Color backgroundColor = Color.FromArgb(20, 20, 20);
+        public static Color white = Color.FromArgb(255, 255, 255);
+    }
+}
