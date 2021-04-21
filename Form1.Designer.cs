2cc939937ff0c7db15de42dacf725a9a996103c4
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-04-20 23:42:39 -0300

    Inserção do formulário de séries e sua respectiva estilização, outras alteração mínimas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,10 @@ namespace ElearningDesktop
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,9 +45,11 @@ namespace ElearningDesktop
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Font = new System.Drawing.Font("Righteous", 28F);
+            this.button1.Location = new System.Drawing.Point(112, 72);
+            this.button1.Margin = new System.Windows.Forms.Padding(10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(280, 104);
             this.button1.TabIndex = 0;
             this.button1.Text = "Série";
             this.button1.UseVisualStyleBackColor = false;
@@ -55,9 +60,11 @@ namespace ElearningDesktop
             this.button2.BackColor = System.Drawing.Color.DimGray;
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(3, 44);
+            this.button2.Font = new System.Drawing.Font("Righteous", 28F);
+            this.button2.Location = new System.Drawing.Point(112, 196);
+            this.button2.Margin = new System.Windows.Forms.Padding(10);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(280, 104);
             this.button2.TabIndex = 1;
             this.button2.Text = "Professores";
             this.button2.UseVisualStyleBackColor = false;
@@ -68,9 +75,11 @@ namespace ElearningDesktop
             this.button3.BackColor = System.Drawing.Color.DimGray;
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(3, 85);
+            this.button3.Font = new System.Drawing.Font("Righteous", 28F);
+            this.button3.Location = new System.Drawing.Point(112, 320);
+            this.button3.Margin = new System.Windows.Forms.Padding(10);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(280, 104);
             this.button3.TabIndex = 2;
             this.button3.Text = "Alunos";
             this.button3.UseVisualStyleBackColor = false;
@@ -81,9 +90,11 @@ namespace ElearningDesktop
             this.button4.BackColor = System.Drawing.Color.DimGray;
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(3, 130);
+            this.button4.Font = new System.Drawing.Font("Righteous", 28F);
+            this.button4.Location = new System.Drawing.Point(112, 444);
+            this.button4.Margin = new System.Windows.Forms.Padding(10);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(280, 104);
             this.button4.TabIndex = 3;
             this.button4.Text = "Turmas";
             this.button4.UseVisualStyleBackColor = false;
@@ -91,26 +102,51 @@ namespace ElearningDesktop
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(0, 80);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.Size = new System.Drawing.Size(1183, 698);
             this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.panel2.Location = new System.Drawing.Point(374, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(704, 623);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ElearningDesktop.Properties.Resources.Group_20;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1117, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1182, 770);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.White;
+            this.IsMdiContainer = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "E-Learning";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,6 +158,8 @@ namespace ElearningDesktop
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,40 +14,53 @@ namespace ElearningDesktop
 {
     public partial class Form1 : Form
     {
-        Color lightGray = Color.FromArgb(80, 80, 80);
-        Color darkGray = Color.FromArgb(61, 61, 61);
         Button oldActiveButton;
 
         public Form1()
         {
             InitializeComponent();
-            oldActiveButton = button1;
-            changeActiveButtonColor(button1);
+
+            this.ForeColor = Colors.white; //cor da classe de cores criadas  
+
             changeButtonFormat(button1);
             changeButtonFormat(button2);
             changeButtonFormat(button3);
             changeButtonFormat(button4);
-            // this.ControlBox = false; //Ocultar barra superior
+
+            oldActiveButton = button1;
+
+            button1.PerformClick();
         }
 
         private void changeButtonFormat(Button button)
         {
             Rectangle rectangle = new Rectangle(0, 0, button.Width, button.Height);
-            GraphicsPath roundedButton = Transform.BorderRadius(rectangle, 15, true, true);
+            GraphicsPath roundedButton = Transform.BorderRadius(rectangle, 18, true, true);
             button.Region = new Region(roundedButton);
         }
 
         private void changeActiveButtonColor(Button newActiveButton)
         {
-            oldActiveButton.BackColor = lightGray;
-            newActiveButton.BackColor = darkGray;
-
+            oldActiveButton.BackColor = Colors.lightGray;
+            newActiveButton.BackColor = Colors.darkGray;
             oldActiveButton = newActiveButton;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             changeActiveButtonColor( (Button)sender);
+
+            Form2 form2 = new Form2();
+            form2.TopLevel = false;
+
+            panel2.Controls.Add(form2);
+
+            form2.Size = new System.Drawing.Size(panel2.Width, panel2.Height);
+            Rectangle rectangle = new Rectangle(0, 0, panel2.Width, panel2.Height);
+            GraphicsPath roundedForm = Transform.BorderRadius(rectangle, 22, true, true);
+            form2.Region = new Region(roundedForm);
+
+            form2.Show();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -62,6 +76,12 @@ namespace ElearningDesktop
         private void button4_Click(object sender, EventArgs e)
         {
             changeActiveButtonColor( (Button)sender);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new System.Drawing.Size(this.Width,80);
+            panel1.Size = new System.Drawing.Size(this.Width, this.Height);
         }
     }
 }
