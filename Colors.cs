def8e49cd7b84667369ac5fafd89096868c0f027
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-04-28 21:37:34 -0300

    implementando métodos e atributos para responsividade

--- Colors.cs
+++ Colors.cs
@@ -14,10 +14,39 @@ namespace ElearningDesktop
         public static Color backgroundColor = Color.FromArgb(20, 20, 20);
         public static Color white = Color.FromArgb(255, 255, 255);
         public static Size formSize;
+        public static Size buttonSize;
+        public static float buttonFontSize;
+        public static Font buttonFont;
+        public static Size panelSize;
 
-        public static void setSize(int width, int height)
+        public static void setFormSize(int FormWidth, int FormHeight)
         {
-            formSize = new Size(width,height);
+            formSize = new Size(FormWidth, FormHeight);
+        }
+
+
+        public static void setButtonSize()
+        {
+            int buttonWidth = Convert.ToInt32(formSize.Width * 0.1);
+            int buttonHeight = Convert.ToInt32(formSize.Height * 0.09);
+
+
+            buttonSize = new Size(buttonWidth, buttonHeight);
+        }
+
+        public static void setButtonFont()
+        {
+            buttonFontSize = ((formSize.Width - formSize.Height) / 30) - 8;
+            buttonFont = new Font("Righteous", buttonFontSize);
+        }
+
+        public static void setPanelSize()
+        {
+            int panelWidth = Convert.ToInt32(formSize.Width);
+            int panelHeight = Convert.ToInt32(formSize.Height);
+
+
+            panelSize = new Size(panelWidth, panelHeight);
         }
     }
 }
