ac2cb8b15341bb9a58e080d31b9e4676f65318f5
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-04-28 21:37:50 -0300

    politicas de clean code

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace ElearningDesktop
@@ -27,14 +28,26 @@ namespace ElearningDesktop
             InitializeComponent();
 
             this.ForeColor = Colors.white; //cor da classe de cores criadas  
+        }
 
+        private void configureButton()
+        {
+            button1.Size = button2.Size = button3.Size = button4.Size = Colors.buttonSize;
+            button1.Font = button2.Font = button3.Font = button4.Font = Colors.buttonFont;
             changeButtonFormat(button1); // chama a função que muda o formato do botão 1
             changeButtonFormat(button2); // chama a função que muda o formato do botão 2
             changeButtonFormat(button3); // chama a função que muda o formato do botão 3
             changeButtonFormat(button4); // chama a função que muda o formato do botão 4
 
-            changePanelFormat(panel2); // chama a função que muda o formato do painel de formulários
-            oldActiveButton = button1; // declara o botão ativo anteriormente como button1, o primeiro a ser aberto
+            changeButtonLocation(button2, button1);
+            changeButtonLocation(button3, button2);
+            changeButtonLocation(button4, button3);
+        }
+
+        private void changeButtonLocation(Button button, Button sourceButton)
+        {
+            button.Location = new Point(sourceButton.Location.X, sourceButton.Location.Y + sourceButton.Size.Height + 30);
+
         }
 
         private void changePanelFormat(Panel panel)
@@ -60,10 +73,10 @@ namespace ElearningDesktop
 
         private void closeOpenedForms() // verifica se tem formulários dessas clases abertos, se tiver fecha
         {
-            if (Application.OpenForms.OfType<Form2>().Count() != 0) form2.Close();
-            if (Application.OpenForms.OfType<Form3>().Count() != 0) form3.Close();
-            if (Application.OpenForms.OfType<Form4>().Count() != 0) form4.Close();
-            if (Application.OpenForms.OfType<Form5>().Count() != 0) form5.Close();
+            if (System.Windows.Forms.Application.OpenForms.OfType<Form2>().Count() != 0) form2.Close();
+            if (System.Windows.Forms.Application.OpenForms.OfType<Form3>().Count() != 0) form3.Close();
+            if (System.Windows.Forms.Application.OpenForms.OfType<Form4>().Count() != 0) form4.Close();
+            if (System.Windows.Forms.Application.OpenForms.OfType<Form5>().Count() != 0) form5.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -123,11 +136,26 @@ namespace ElearningDesktop
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            button1.PerformClick(); // simula um clique via código
             panel1.Size = new System.Drawing.Size(this.Width, this.Height);
             panel3.Size = new System.Drawing.Size(this.Width, 90);
-            Colors.setSize(this.Width,this.Height);
-            button1.Size = new Size( Convert.ToInt32(Colors.formSize.Width * 0.1), Convert.ToInt32(Colors.formSize.Height * 0.1));
+
+
+            Colors.setFormSize(this.Width, this.Height);
+            Colors.setButtonSize();
+            Colors.setButtonFont();
+            Colors.setPanelSize();
+
+
+            configureButton();
+
+            changePanelFormat(panel2); // chama a função que muda o formato do painel de formulários
+
+            panel2.Location = new Point(button1.Location.X + button1.Width, button1.Location.Y); // muda a posição do painel de formulários
+            panel2.Size = Colors.panelSize;
+
+            oldActiveButton = button1; // declara o botão ativo anteriormente como button1, o primeiro a ser aberto
+
+            button1.PerformClick(); // simula um clique via código
         }
     }
 }
