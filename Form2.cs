d5da4f7ec1812738ed43887f53bef39eed05721c
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-07 16:07:27 -0300

    criando pictureBox com a imagem da série

--- Form2.cs
+++ Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,13 @@ namespace ElearningDesktop
             InitializeComponent();
             this.ControlBox = false; //Ocultar barra superior
             this.BackColor = Styles.darkGray;
-            this.ForeColor = Styles.white; //cor da classe de cores criada
+            this.ForeColor = Styles.white; //cor da classe de estilos criada
             seriesCounter = 0;
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            this.Controls.Add( createSerie() );
+            this.Controls.Add( createSerie() );// adiciona ao form, o panel criado 
             this.Controls.Add( createSerie() );
             this.Controls.Add( createSerie() );
             this.Controls.Add( createSerie() );
@@ -50,24 +51,51 @@ namespace ElearningDesktop
 
         private Panel createSerie()
         {
-            Panel serie = new Panel();
-            serie.Size = Styles.seriesSize;
+            int gradeNumber = 1;
 
-            serie.Location = new Point(20, 20 + (20 + Styles.seriesSize.Height) * seriesCounter);
+            Panel serie = new Panel(); //cria uma div
+            serie.Size = Styles.seriesSize; //define o tamanho da div
 
-            serie.BackColor = Styles.backgroundColor;
+            serie.Location = new Point(20, 20 + (20 + Styles.seriesSize.Height) * seriesCounter); // define a posição da div
 
-            Label classroom = new Label();
-            classroom.Text = "INF Diurno";
-            classroom.Font = Styles.buttonFont;
-            classroom.Size = new Size(Convert.ToInt32(classroom.Text.Length * Styles.buttonFontSize),Convert.ToInt32(Styles.buttonFontSize * 2));
-            classroom.TextAlign = ContentAlignment.MiddleLeft;
+            serie.BackColor = Styles.backgroundColor; //define a cor preta para fundo da div
 
-            changePanelFormat(serie);
+            PictureBox gradePicture = new PictureBox();
 
-            serie.Controls.Add(classroom);
+            switch (gradeNumber)
+            {
+                case 1:
+                    gradePicture.Image = Properties.Resources.num1;
+                    break;
 
-            seriesCounter++;
+                case 2:
+                    gradePicture.Image = Properties.Resources.num2;
+                    break;
+
+                case 3:
+                    gradePicture.Image = Properties.Resources.num3;
+                    break;
+            }
+
+            gradePicture.Size = new Size(Convert.ToInt32(Styles.seriesSize.Width * 0.05), Convert.ToInt32(Styles.seriesSize.Height * 0.6));
+            gradePicture.SizeMode = PictureBoxSizeMode.StretchImage;
+            gradePicture.Location = new Point(Convert.ToInt32(serie.Location.X + 2), Convert.ToInt32((serie.Size.Height / 2) - (gradePicture.Size.Height / 2)));
+
+
+            serie.Controls.Add(gradePicture);//adiciona o pictureBox na div
+
+            Label classroom = new Label(); //cria a serie
+            classroom.Text = "INF Diurno"; //define o nome da serie
+            classroom.Font = Styles.defaultFont;//define a estilização do texto
+            classroom.Size = new Size(Convert.ToInt32(classroom.Text.Length * Styles.buttonFontSize),Convert.ToInt32(Styles.buttonFontSize * 2));//define o tamanho que o texto ocupa na div
+            classroom.TextAlign = ContentAlignment.MiddleLeft; //alinha o texto ao centro(x) centro(y)
+            classroom.Location = new Point(Convert.ToInt32(gradePicture.Location.X + gradePicture.Size.Width + 10), Convert.ToInt32((serie.Size.Height / 2) - (serie.Size.Height / 2)));
+
+            serie.Controls.Add(classroom);//adiciona o label na div
+
+            changePanelFormat(serie);//arredonda cada div
+
+            seriesCounter++; //aumenta em 1 a quantidade 
             return serie;
         }
 
