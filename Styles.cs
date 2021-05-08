581946a51a5edc70bcc24587dcd6a3d2941b1cd9
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-07 22:13:21 -0300

    Utilizando as novas fontes

--- Styles.cs
+++ Styles.cs
@@ -2,9 +2,12 @@
 using System.Activities;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ElearningDesktop
 {
@@ -29,6 +32,8 @@ namespace ElearningDesktop
         public static Font customFont;
         public static float customFontLetterSize;
 
+        public static PrivateFontCollection usedFonts;
+
         public static void setFormSize(int FormWidth, int FormHeight)
         {
             formSize = new Size(FormWidth, FormHeight);
@@ -50,7 +55,7 @@ namespace ElearningDesktop
         public static void setDefaultFont()
         {
             defaultFontLetterSize = Convert.ToInt32(formSize.Width / 72) ;
-            defaultFont = new Font("Righteous", defaultFontLetterSize);
+            defaultFont = new Font(usedFonts.Families[1], defaultFontLetterSize);
         }
 
         public static void setMainPanelSize()
@@ -75,7 +80,14 @@ namespace ElearningDesktop
         public static void setCustomFont()
         {
             customFontLetterSize = Convert.ToInt32(formSize.Width / 120);
-            customFont = new Font("Quicksand", customFontLetterSize);
+            customFont = new Font(usedFonts.Families[0], customFontLetterSize);
+        }
+
+        public static void setProjectFonts()
+        {
+            usedFonts = new PrivateFontCollection();
+            usedFonts.AddFontFile(Path.Combine(System.Windows.Forms.Application.StartupPath, "Quicksand-Regular.ttf"));
+            usedFonts.AddFontFile(Path.Combine(System.Windows.Forms.Application.StartupPath, "Righteous-Regular.ttf"));
         }
     }
 }
