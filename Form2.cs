1536e69bcefdc35ec0c34df43e4c2fdfceb81aa0
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-13 14:33:51 -0300

    Corrigindo responsividade dos checkboxes
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- Form2.cs
+++ Form2.cs
@@ -54,10 +54,11 @@ namespace ElearningDesktop
 
             int checkBoxCount = filterPanel.Controls.OfType<CheckBox>().ToArray().Count();
             CheckBox[] checkBoxArray = filterPanel.Controls.OfType<CheckBox>().ToArray();
+            Array.Reverse(checkBoxArray,0,checkBoxCount); // inverte os elementos do array
             //Label[] labelArray = filterPanel.Controls.OfType<Label>().ToArray();
             //int labelCount = filterPanel.Controls.OfType<Label>().ToArray().Count();
-            int heightNeeded = filterPanel.Location.Y + 10;
-            for(int i = 0;i <= checkBoxCount-1; i++)
+            int heightNeeded = filterPanel.Location.Y ;
+            for(int i = 0; i <= checkBoxCount - 1; i++)
             {
                 switch (i)
                 {
@@ -74,14 +75,11 @@ namespace ElearningDesktop
                         heightNeeded += label3.Height;
                         break;
                 }
-                checkBoxArray[checkBoxCount - i - 1].Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints * 0.625));
-                checkBoxArray[checkBoxCount - i - 1].Location = new Point(10 , heightNeeded);
+                //checkBoxArray[checkBoxCount - i - 1].Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints * 0.625));
+                //checkBoxArray[checkBoxCount - i - 1].Location = new Point(10 , heightNeeded);
+                checkBoxArray[i].Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints * 0.625));
+                checkBoxArray[i].Location = new Point(10 , heightNeeded);
                 heightNeeded += checkBoxArray[i].Height; 
-
-                /*if(i <= 2) checkBoxArray[i].Location = c
-                else if(i <= 5) checkBoxArray[i].Location = new Point(label2.Location.X, label2.Location.Y + (label2.Height + 10) * i);
-                else checkBoxArray[i].Location = new Point(label3.Location.X, label3.Location.Y + (label3.Height + 10) * i);*/
-
             }
 
         #region series
