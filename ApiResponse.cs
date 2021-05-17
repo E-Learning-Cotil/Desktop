9fac2f2e75bde50dd0bf4154171ec519ed4acb81
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-17 15:39:46 -0300

    Series API Restful response
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

---
+++ ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElearningDesktop
+{
+    class ApiResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("curso")]
+        public string Curso { get; set; }
+
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+
+        [JsonProperty("ano")]
+        public int Ano { get; set; }
+
+        [JsonProperty("periodo")]
+        public string Periodo { get; set; }
+    }
+}
