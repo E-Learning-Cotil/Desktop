a25d76686b74f78a9e690c1ea845fc004f1afff8
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-17 15:40:31 -0300

    Interface Conexão com API
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

---
+++ ApiService.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElearningDesktop
+{
+    interface ApiService
+    {
+        [Get("/series/list/")]
+        Task<dynamic> GetSeriesAsync();
+    }
+}
