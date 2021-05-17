1dac0a49ecdaf73fa6f84bcd105393b76292c8eb
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-17 18:02:38 -0300

    Create Count.cs
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

---
+++ Count.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElearningDesktop
+{
+    class Count
+    {
+        [JsonProperty("turmas")]
+        public int Turmas { get; set; }
+    }
+}
