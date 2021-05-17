ab1d80f6570ac8c99ae0f7e5e8a0baa95ef922f8
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-17 15:42:48 -0300

    Integrando API para dinamicamente criar séries
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,7 +16,6 @@ namespace ElearningDesktop
 {
     public partial class Form2 : Form
     {
-        int seriesCounter;
 
         public Form2()
         {
@@ -22,7 +23,6 @@ namespace ElearningDesktop
             this.ControlBox = false; //Ocultar barra superior
             this.BackColor = Styles.darkGray;
             this.ForeColor = Styles.white; //cor da classe de estilos criada
-            seriesCounter = 0;
         }
 
         private void filterButtonStyle()
@@ -65,23 +65,8 @@ namespace ElearningDesktop
             filterPosition();
             checkboxStyle();
             filterButtonStyle();
-
-            #region Criando Series
+            
             createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            createSerie();
-            #endregion
 
         }
 
@@ -119,74 +104,40 @@ namespace ElearningDesktop
             }
         }
 
-        private void createSerie()
+        private async void createSerie()
         {
-            int gradeNumber = 3;
-            int serieQuantity = seriesCounter;
-
-            Panel seriePanel = new Panel(); //cria uma div
-            seriePanel.Size = Styles.seriesSize; //define o tamanho da div
-
-            seriePanel.Location = new Point(20, 20 + (20 + Styles.seriesSize.Height) * seriesCounter); // define a posição da div
-
-            seriePanel.BackColor = Styles.backgroundColor; //define a cor preta para fundo da div
-
-            PictureBox gradePicture = new PictureBox();
-
-            switch (gradeNumber)
+            try
             {
-                case 1:
-                    gradePicture.Image = Properties.Resources.num1;
-                    break;
+                var apiPath = RestService.For<ApiService>("https://elearning-tcc.herokuapp.com");
+                var dataResponse = await apiPath.GetSeriesAsync();
 
-                case 2:
-                    gradePicture.Image = Properties.Resources.num2;
-                    break;
+                ApiResponse[] series = JsonConvert.DeserializeObject<ApiResponse[]>(dataResponse.ToString());
 
-                case 3:
-                    gradePicture.Image = Properties.Resources.num3;
-                    break;
+                if(series.Length == 0)
+                {
+                    Label noSeries = new Label();
+                    noSeries.Text = "Não há turmas cadastradas!";
+                    noSeries.Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints));
+                    noSeries.AutoSize = true;
+                    noSeries.Location = new Point(20, 20);
+                    seriesPanel.Controls.Add(noSeries);
+                }
+                else
+                {
+                    for (int i = 0; i < series.Length; i++)
+                    {
+                        ApiResponse serieData = series[i];
+                        Series serie = new Series(serieData.Id, serieData.Curso, serieData.Tipo, serieData.Ano, serieData.Periodo);
+                        seriesPanel.Controls.Add(serie.getSeriePanel());
+                        seriesPanel.Size = new Size(seriesPanel.Width, seriesPanel.Height - 1);
+                    }
+                }
+
             }
-
-            gradePicture.Size = new Size(Convert.ToInt32(Styles.seriesSize.Width * 0.05), Convert.ToInt32(Styles.seriesSize.Height * 0.6));
-            gradePicture.SizeMode = PictureBoxSizeMode.StretchImage;
-            gradePicture.Location = new Point(Convert.ToInt32(seriePanel.Location.X + 2), Convert.ToInt32((seriePanel.Size.Height / 2) - (gradePicture.Size.Height / 2)));
-
-
-            seriePanel.Controls.Add(gradePicture);//adiciona o pictureBox na div
-
-            Label serieName = new Label(); //cria a serie
-            serieName.Text = "INF Diurno"; //define o nome da serie
-            serieName.Font = Styles.defaultFont;//define a estilização do texto
-            serieName.AutoSize = true;
-            serieName.TextAlign = ContentAlignment.MiddleLeft; //alinha o texto ao centro(x) centro(y)
-            serieName.Location = new Point(Convert.ToInt32(gradePicture.Location.X + gradePicture.Size.Width + 10), Convert.ToInt32((seriePanel.Size.Height / 2) - (serieName.Font.Height / 2)));
-
-            seriePanel.Controls.Add(serieName);//adiciona o label na div
-
-            Label assignedClass = new Label();
-            assignedClass.Text = serieQuantity + " turmas atribuídas";
-            assignedClass.Font = Styles.customFont;//define a estilização do texto
-
-            assignedClass.AutoSize = true;
-            assignedClass.TextAlign = ContentAlignment.MiddleLeft;
-
-            assignedClass.Location = new Point(seriePanel.Width - Convert.ToInt32(Styles.formSize.Width * 0.115), Convert.ToInt32((seriePanel.Size.Height / 2) - (assignedClass.Font.Height / 2)));
-            seriePanel.Controls.Add(assignedClass);
-             
-            changePanelFormat(seriePanel);//arredonda cada div
-
-            seriesPanel.Controls.Add(seriePanel);
-            seriesPanel.Size = new Size(seriesPanel.Width, seriesPanel.Height - 1);
-
-            seriesCounter++; //aumenta em 1 a quantidade 
-        }
-
-        private void changePanelFormat(Panel panel)
-        {
-            Rectangle rectangle = new Rectangle(0, 0, panel.Width, panel.Height);
-            GraphicsPath roundedPanel = Transform.BorderRadius(rectangle, 13, true, true, true, true);
-            panel.Region = new Region(roundedPanel);
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro na Consulta: " + ex.Message);
+            }
         }
 
     }
