bfe8489aead8d6c5e6bda5e860632d85f27abe85
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-25 19:56:12 -0300

    Criando arquivo para armazenar as rotas do projeto
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

---
+++ Routes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElearningDesktop
+{
+    public static class Routes
+    {
+        public static string baseUrl = "https://elearning-tcc.herokuapp.com";
+        //public static string baseUrl = "http://50f46cb72afc.ngrok.io";// rota de teste
+        public static string listSeries = "/series/list";
+        public static string listFilter = "";
+    }
+}
