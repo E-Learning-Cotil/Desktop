525f39b2ff47b73e439157c28d57e76f721e9f22
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-25 19:56:00 -0300

    Criando classe esqueleto para GET na API
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- ApiService.cs
+++ ApiService.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 
 namespace ElearningDesktop
 {
+    public class QueryParameters
+    {
+        public string curso { get; set; }
+        public string ano { get; set; }
+        public string tipo { get; set; }
+        public string periodo { get; set; }
+    }
+
+
     interface ApiService
     {
         [Get("/series/list/")]
         Task<dynamic> GetSeriesAsync();
+
+        [Get("/series/list/")]
+        Task<dynamic> GetSeriesFilteredAsync(QueryParameters parametros);
     }
 }
