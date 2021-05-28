0f28f524732faeecadf669cb4983b62f2a894649
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-05-28 19:20:02 -0300

    Iniciando a criação da página de professores
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,36 +29,225 @@ namespace ElearningDesktop
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.teachersPanel = new System.Windows.Forms.Panel();
+            this.loadingCircle1 = new MRG.Controls.UI.LoadingCircle();
+            this.loadingText = new System.Windows.Forms.Label();
+            this.plusButtonPictureBox = new System.Windows.Forms.PictureBox();
+            this.filterPanel = new System.Windows.Forms.Panel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.linePanel = new System.Windows.Forms.Panel();
+            this.filterButtonPanel = new System.Windows.Forms.Panel();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.telephoneTextBox = new System.Windows.Forms.TextBox();
+            this.telephoneLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.teachersPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.plusButtonPictureBox)).BeginInit();
+            this.filterPanel.SuspendLayout();
+            this.filterButtonPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // teachersPanel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Formulário de professores";
+            this.teachersPanel.AutoScroll = true;
+            this.teachersPanel.Controls.Add(this.loadingCircle1);
+            this.teachersPanel.Controls.Add(this.loadingText);
+            this.teachersPanel.Location = new System.Drawing.Point(12, 12);
+            this.teachersPanel.Name = "teachersPanel";
+            this.teachersPanel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
+            this.teachersPanel.Size = new System.Drawing.Size(485, 420);
+            this.teachersPanel.TabIndex = 4;
+            // 
+            // loadingCircle1
+            // 
+            this.loadingCircle1.Active = true;
+            this.loadingCircle1.BackColor = System.Drawing.Color.Transparent;
+            this.loadingCircle1.Color = System.Drawing.Color.DarkGray;
+            this.loadingCircle1.InnerCircleRadius = 8;
+            this.loadingCircle1.Location = new System.Drawing.Point(17, 10);
+            this.loadingCircle1.Name = "loadingCircle1";
+            this.loadingCircle1.NumberSpoke = 24;
+            this.loadingCircle1.OuterCircleRadius = 9;
+            this.loadingCircle1.RotationSpeed = 20;
+            this.loadingCircle1.Size = new System.Drawing.Size(33, 23);
+            this.loadingCircle1.SpokeThickness = 4;
+            this.loadingCircle1.StylePreset = MRG.Controls.UI.LoadingCircle.StylePresets.IE7;
+            this.loadingCircle1.TabIndex = 1;
+            this.loadingCircle1.Text = "loadingCircle1";
+            // 
+            // loadingText
+            // 
+            this.loadingText.AutoSize = true;
+            this.loadingText.Font = new System.Drawing.Font("Wide Latin", 8.25F, System.Drawing.FontStyle.Bold);
+            this.loadingText.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.loadingText.Location = new System.Drawing.Point(98, 19);
+            this.loadingText.Name = "loadingText";
+            this.loadingText.Size = new System.Drawing.Size(150, 14);
+            this.loadingText.TabIndex = 0;
+            this.loadingText.Text = "Carregando ...";
+            // 
+            // plusButtonPictureBox
+            // 
+            this.plusButtonPictureBox.BackColor = System.Drawing.Color.White;
+            this.plusButtonPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.plusButtonPictureBox.Image = global::ElearningDesktop.Properties.Resources.plus;
+            this.plusButtonPictureBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.plusButtonPictureBox.Location = new System.Drawing.Point(447, 411);
+            this.plusButtonPictureBox.Name = "plusButtonPictureBox";
+            this.plusButtonPictureBox.Padding = new System.Windows.Forms.Padding(15);
+            this.plusButtonPictureBox.Size = new System.Drawing.Size(50, 50);
+            this.plusButtonPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.plusButtonPictureBox.TabIndex = 5;
+            this.plusButtonPictureBox.TabStop = false;
+            this.plusButtonPictureBox.Click += new System.EventHandler(this.plusButtonPictureBox_Click);
+            // 
+            // filterPanel
+            // 
+            this.filterPanel.AutoScroll = true;
+            this.filterPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.filterPanel.Controls.Add(this.emailTextBox);
+            this.filterPanel.Controls.Add(this.emailLabel);
+            this.filterPanel.Controls.Add(this.telephoneTextBox);
+            this.filterPanel.Controls.Add(this.telephoneLabel);
+            this.filterPanel.Controls.Add(this.nameTextBox);
+            this.filterPanel.Controls.Add(this.nameLabel);
+            this.filterPanel.Location = new System.Drawing.Point(503, 12);
+            this.filterPanel.Name = "filterPanel";
+            this.filterPanel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
+            this.filterPanel.Size = new System.Drawing.Size(225, 430);
+            this.filterPanel.TabIndex = 6;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.nameLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.nameLabel.Location = new System.Drawing.Point(3, 10);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Nome:";
+            // 
+            // linePanel
+            // 
+            this.linePanel.BackColor = System.Drawing.Color.White;
+            this.linePanel.Location = new System.Drawing.Point(503, 446);
+            this.linePanel.Name = "linePanel";
+            this.linePanel.Size = new System.Drawing.Size(225, 10);
+            this.linePanel.TabIndex = 44;
+            // 
+            // filterButtonPanel
+            // 
+            this.filterButtonPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.filterButtonPanel.Controls.Add(this.filterButton);
+            this.filterButtonPanel.Location = new System.Drawing.Point(503, 446);
+            this.filterButtonPanel.Name = "filterButtonPanel";
+            this.filterButtonPanel.Size = new System.Drawing.Size(225, 61);
+            this.filterButtonPanel.TabIndex = 43;
+            // 
+            // filterButton
+            // 
+            this.filterButton.BackColor = System.Drawing.Color.White;
+            this.filterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.filterButton.ForeColor = System.Drawing.Color.Black;
+            this.filterButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.filterButton.Location = new System.Drawing.Point(79, 19);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(75, 23);
+            this.filterButton.TabIndex = 2;
+            this.filterButton.Text = "Filtrar";
+            this.filterButton.UseVisualStyleBackColor = false;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(6, 26);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(173, 20);
+            this.nameTextBox.TabIndex = 43;
+            // 
+            // telephoneTextBox
+            // 
+            this.telephoneTextBox.Location = new System.Drawing.Point(6, 75);
+            this.telephoneTextBox.Name = "telephoneTextBox";
+            this.telephoneTextBox.Size = new System.Drawing.Size(173, 20);
+            this.telephoneTextBox.TabIndex = 45;
+            // 
+            // telephoneLabel
+            // 
+            this.telephoneLabel.AutoSize = true;
+            this.telephoneLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.telephoneLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.telephoneLabel.Location = new System.Drawing.Point(3, 59);
+            this.telephoneLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.telephoneLabel.Name = "telephoneLabel";
+            this.telephoneLabel.Size = new System.Drawing.Size(52, 13);
+            this.telephoneLabel.TabIndex = 44;
+            this.telephoneLabel.Text = "Telefone:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(6, 126);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(173, 20);
+            this.emailTextBox.TabIndex = 47;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.emailLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.emailLabel.Location = new System.Drawing.Point(3, 110);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(38, 13);
+            this.emailLabel.TabIndex = 46;
+            this.emailLabel.Text = "E-mail:";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(740, 571);
+            this.Controls.Add(this.linePanel);
+            this.Controls.Add(this.filterButtonPanel);
+            this.Controls.Add(this.filterPanel);
+            this.Controls.Add(this.plusButtonPictureBox);
+            this.Controls.Add(this.teachersPanel);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form3";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.teachersPanel.ResumeLayout(false);
+            this.teachersPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.plusButtonPictureBox)).EndInit();
+            this.filterPanel.ResumeLayout(false);
+            this.filterPanel.PerformLayout();
+            this.filterButtonPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel teachersPanel;
+        private MRG.Controls.UI.LoadingCircle loadingCircle1;
+        private System.Windows.Forms.Label loadingText;
+        private System.Windows.Forms.PictureBox plusButtonPictureBox;
+        private System.Windows.Forms.Panel filterPanel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox telephoneTextBox;
+        private System.Windows.Forms.Label telephoneLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Panel linePanel;
+        private System.Windows.Forms.Panel filterButtonPanel;
+        private System.Windows.Forms.Button filterButton;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +15,181 @@ namespace ElearningDesktop
 {
     public partial class Form3 : Form
     {
-        public Form3()
+        #region Declaração de Variáveis
+        TeachersApiResponse[] teachers;
+
+        Form1 parentForm = null;
+        #endregion
+
+        public Form3(Form1 parentForm)
         {
             InitializeComponent();
-
             this.ControlBox = false; //Ocultar barra superior
-
             this.BackColor = Styles.darkGray;
             this.ForeColor = Styles.white;
+            this.parentForm = parentForm;
+        }
+
+        private void filterButtonStyle()
+        {
+            filterButton.Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints * 0.7));
+            filterButton.Size = new Size(Convert.ToInt32(filterButtonPanel.Width * 0.830), Convert.ToInt32(filterButtonPanel.Height * 0.574));
+            filterButton.Location = new Point(Convert.ToInt32(filterButtonPanel.Width / 2 - filterButton.Width / 2), Convert.ToInt32(filterButtonPanel.Height / 2 - filterButton.Height / 2));
+            Rectangle rectangle = new Rectangle(0, 0, filterButton.Width, filterButton.Height);
+            GraphicsPath roundedButton = Transform.BorderRadius(rectangle, 20, true, true, true, true);
+            filterButton.Region = new Region(roundedButton);
+        }
+
+        private void filterPosition()
+        {
+            filterPanel.Size = new Size(Convert.ToInt32(this.Width * 0.263), Convert.ToInt32(this.Height * 0.825));
+            filterPanel.Location = new Point(Convert.ToInt32(this.Width * 0.712), Convert.ToInt32(this.Height * 0.043));
+            Rectangle rectangle = new Rectangle(0, 0, filterPanel.Width, filterPanel.Height);
+            GraphicsPath roundedPanel = Transform.BorderRadius(rectangle, 20, true, true, false, false);
+            filterPanel.Region = new Region(roundedPanel);
+
+            filterButtonPanel.Location = new Point(filterPanel.Location.X, Convert.ToInt32(filterPanel.Height + filterPanel.Location.Y));
+            filterButtonPanel.Size = new Size(Convert.ToInt32(this.Width * 0.263), Convert.ToInt32(this.Height * 0.103));
+            rectangle = new Rectangle(0, 0, filterButtonPanel.Width, filterButtonPanel.Height);
+            roundedPanel = Transform.BorderRadius(rectangle, 20, false, false, true, true);
+            filterButtonPanel.Region = new Region(roundedPanel);
+
+            linePanel.Location = new Point(filterButtonPanel.Location.X, filterButtonPanel.Location.Y);
+            linePanel.Size = new Size(filterButtonPanel.Width, 2);
+
+            teachersPanel.Location = new Point(0, 7);
+            teachersPanel.Size = new Size(Convert.ToInt32(this.Width * 0.673), this.Height);
+
+            nameLabel.Font = telephoneLabel.Font = emailLabel.Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints * 0.875));
+            nameLabel.ForeColor = telephoneLabel.ForeColor = emailLabel.ForeColor = Styles.filterTitleColor;
+
+            nameTextBox.Font = telephoneTextBox.Font = emailTextBox.Font = new Font(Styles.customFont.FontFamily, Convert.ToInt32(Styles.customFont.SizeInPoints * 0.75));
+            nameTextBox.Size = telephoneTextBox.Size = emailTextBox.Size = new Size(filterPanel.Width - 20, Convert.ToInt32(Styles.customFont.SizeInPoints * 0.75));
+
+            nameLabel.Location = new Point(10, 20);
+            nameTextBox.Location = new Point(10, nameLabel.Location.Y + nameLabel.Height + 10);
+
+            telephoneLabel.Location = new Point(10, nameTextBox.Location.Y + nameTextBox.Height + 10);
+            telephoneTextBox.Location = new Point(10, telephoneLabel.Location.Y + telephoneLabel.Height + 10);
+
+            emailLabel.Location = new Point(10, telephoneTextBox.Location.Y + telephoneTextBox.Height + 10);
+            emailTextBox.Location = new Point(10, emailLabel.Location.Y + emailLabel.Height + 10);
+        }
+
+        private void loadingMessageStyle()
+        {
+            loadingText.Font = Styles.defaultFont;
+
+            loadingCircle1.Location = new Point(Convert.ToInt32((teachersPanel.Width / 2) - (loadingCircle1.Width / 2)), Convert.ToInt32(this.Height / 2 - loadingCircle1.Height / 2));
+
+            loadingText.Location = new Point(Convert.ToInt32((teachersPanel.Width / 2) - (loadingText.Width / 2)) + 10, loadingCircle1.Location.Y - loadingText.Height - 10);
+        }
+
+        private void stylePlusButton()
+        {
+            plusButtonPictureBox.Size = new Size(Convert.ToInt32(this.Height * 0.083), Convert.ToInt32(this.Height * 0.083));
+
+            plusButtonPictureBox.Location = new Point(Styles.seriesSize.Width + 20 - plusButtonPictureBox.Width, teachersPanel.Height - plusButtonPictureBox.Height - 7);
+
+            Rectangle rectangle = new Rectangle(0, 0, plusButtonPictureBox.Width, plusButtonPictureBox.Height);
+            GraphicsPath roundedButton = Transform.BorderRadius(rectangle, 60, true, true, true, true);
+            plusButtonPictureBox.Region = new Region(roundedButton);
+        }
+
+        private async void listTeachers(TeacherQueryParameters filters)
+        {
+            try
+            {
+                var apiPath = RestService.For<ApiService>(Routes.baseUrl);
+                if (filters == null)
+                {
+                    var dataResponse = await apiPath.GetTeachersAsync();
+                    teachers = JsonConvert.DeserializeObject<TeachersApiResponse[]>(dataResponse.ToString());
+                }
+                else
+                {
+                    var dataResponse = await apiPath.GetTeachersFilteredAsync(filters);
+                    teachers = JsonConvert.DeserializeObject<TeachersApiResponse[]>(dataResponse.ToString());
+                }
+
+                loadingText.Visible = false;
+                loadingCircle1.Visible = false;
+
+                if (teachers.Length == 0)
+                {
+                    Label noTeachers = new Label();
+                    noTeachers.Name = "noTeachers";
+                    noTeachers.Text = "Não há professores cadastrados!";
+                    noTeachers.Font = new Font(Styles.defaultFont.FontFamily, Convert.ToInt32(Styles.defaultFont.SizeInPoints));
+                    noTeachers.AutoSize = true;
+                    noTeachers.Location = new Point(20, 20);
+                    teachersPanel.Controls.Add(noTeachers);
+                }
+                else
+                {
+                    for (int i = 0; i < teachers.Length; i++)
+                    {
+                        TeachersApiResponse teachersData = teachers[i];
+
+                        Teachers serie = new Teachers(teachersData.Nome, teachersData.Telefone, teachersData.Email,teachersData.RG, teachersData.Foto,i);
+                        teachersPanel.Controls.Add(serie.getSeriePanel());
+                        teachersPanel.Size = new Size(teachersPanel.Width, teachersPanel.Height - 1);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro durante a conexão com a base de dados. " + ex.Message);
+            }
+    }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            filterPosition();
+            filterButtonStyle();
+
+            stylePlusButton();
+
+            loadingMessageStyle();
+
+            listTeachers(null);
+        }
+
+        private void plusButtonPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            int itemsCount = teachersPanel.Controls.Count;
+            for (int i = itemsCount - 1; i > 0; i--)
+            {
+                if (teachersPanel.Controls[i].Name == "noTeachers")
+                {
+                    teachersPanel.Controls.Remove(teachersPanel.Controls[i]);
+                    continue;
+                }
+
+                Type objectType = teachersPanel.Controls[i].GetType();
+
+                if (objectType == typeof(Panel))
+                {
+                    teachersPanel.Controls.Remove(teachersPanel.Controls[i]);
+                    itemsCount--;
+                }
+            }
+
+            loadingText.Visible = true;
+            loadingCircle1.Visible = true;
+
+            TeacherQueryParameters filters = new TeacherQueryParameters();
+            if (nameTextBox.Text.Trim() != "") filters.nome = nameTextBox.Text;
+            if (telephoneTextBox.Text.Trim() != "") filters.telefone = telephoneTextBox.Text;
+            if (emailTextBox.Text.Trim() != "") filters.email = emailTextBox.Text;
+
+            listTeachers(filters);
         }
     }
 }
