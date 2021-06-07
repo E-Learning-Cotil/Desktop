88c523695b2d90477af7701328247c801b742076
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-06-07 18:48:41 -0300

    Conectando a imagem de url inválida
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -169,5 +169,15 @@ namespace ElearningDesktop.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap x {
+            get {
+                object obj = ResourceManager.GetObject("x", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
