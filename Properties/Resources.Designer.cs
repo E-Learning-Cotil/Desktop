4c58bb0ff7db64d7ae666179ce82c83a119dd1c0
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-06-08 11:49:18 -0300

    CORE elements
    Co-Authored-By: Amazing512 <60967298+Amazing512@users.noreply.github.com>

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace ElearningDesktop.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap upload {
+            get {
+                object obj = ResourceManager.GetObject("upload", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Vector {
             get {
                 object obj = ResourceManager.GetObject("Vector", resourceCulture);
