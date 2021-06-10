825e8edafb1f8c228f7c2c99d56407583b5cfaf4
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-06-10 18:09:01 -0300

    Atualizando rota de armazenamento de fotos

--- Routes.cs
+++ Routes.cs
@@ -11,7 +11,7 @@ namespace ElearningDesktop
         public static string baseUrl = "https://elearning-tcc.herokuapp.com";
         //public static string baseUrl = "http://50f46cb72afc.ngrok.io";// rota de teste
         public static string listSeries = "/series/list";
-        public static string sendImageBaseUrl = "https://b2-img-upload.herokuapp.com";
+        public static string sendImageBaseUrl = "https://elearning-tcc.herokuapp.com";
         public static string listFilter = "";
     }
 }
